035247ac5653edd29c3aa0289cbb66bbcd8daf51
Ariel <r_e_l_g@yahoo.com>
2023-12-17 19:49:30 +0200

    Update provider.tf

--- demo-INclass/provider.tf
+++ demo-INclass/provider.tf
@@ -1,6 +1,6 @@
 # COnfigure the AWS PROVIDER
 provider "aws" {
-    access_key = "AKIAU77SCMU3IVJGCAGC"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region     = "eu-west-2"
 }
