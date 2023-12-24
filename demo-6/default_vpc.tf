707f37a96a14699c1bba9c81fb3436349dd5efc4
Ariel Guez <r_e_l_g@yahoo.com>
2023-12-24 10:17:05 +0200

    default_vpc update

--- demo-6/default_vpc.tf
+++ demo-6/default_vpc.tf
@@ -13,27 +13,27 @@ resource "aws_default_vpc" "default" {
 # creates 3 machines(according to the default specifically)
 # for each AZ(Azone)
 resource "aws_default_subnet" "default_az1" {
-    availability_zone = "${var.AWS_REGION}"
+    availability_zone = "${var.AWS_REGION}a"
 
     tags  = {
         ManagedBy = "terraform"
-        Name      = "default Subnet for ${var.AWS_REGION} a"
+        Name      = "default Subnet for ${var.AWS_REGION}a"
     }
 }
 resource "aws_default_subnet" "default_az2" {
-    availability_zone = "${var.AWS_REGION}"
+    availability_zone = "${var.AWS_REGION}b"
 
     tags  = {
         ManagedBy = "terraform"
-        Name      = "default Subnet for ${var.AWS_REGION} b"
+        Name      = "default Subnet for ${var.AWS_REGION}b"
     }
 }
 
 resource "aws_default_subnet" "default_az3" {
-    availability_zone = "${var.AWS_REGION}"
+    availability_zone = "${var.AWS_REGION}c"
 
     tags  = {
         ManagedBy = "terraform"
-        Name      = "default Subnet for ${var.AWS_REGION} c"
+        Name      = "default Subnet for ${var.AWS_REGION}c"
     }
 }
